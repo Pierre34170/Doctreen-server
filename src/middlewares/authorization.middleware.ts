e9928bf46200da3700ba891ed3ec5483aa989cca
Pierre34170 <perr1.pierre34@gmail.com>
2022-02-07 08:25:20 +0100

    clean code

--- src/middlewares/authorization.middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Response } from 'express';
-import jwt from 'jsonwebtoken';
-import HttpException from '../exceptions/HttpException';
-import { DataStoredInToken, RequestWithUser } from '../interfaces/auth.interface';
-import DB from '../db';
-
-export const requireModeratorLogin = async (req: RequestWithUser, res: Response, next: NextFunction) => {
-    
-}
